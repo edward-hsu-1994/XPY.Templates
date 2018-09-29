df772817d6811f403f29e23be50ba6211fac86b5
XuPeiYao <st89382000@gmail.com>
2018-09-29 20:35:57 +0800

    加入取代參數

---
+++ XPY.Templates/WizardImplementation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace XPY.Templates.Web.Wizard {
+    public class WizardImplementation : IWizard {
+        public void BeforeOpeningFile(ProjectItem projectItem) {
+        }
+
+        public void ProjectFinishedGenerating(Project project) {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem) {
+        }
+
+        public void RunFinished() {
+
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams) {
+            replacementsDictionary["$lastnamespace$"] = replacementsDictionary["$safeprojectname$"].Split('.').Last();
+            replacementsDictionary["$safeclassname$"] = replacementsDictionary["$safeprojectname$"].Replace(".", "");
+        }
+
+        public bool ShouldAddProjectItem(string filePath) {
+            return true;
+        }
+    }
+}
