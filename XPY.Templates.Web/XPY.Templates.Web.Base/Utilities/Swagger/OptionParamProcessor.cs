51534dc0a1b6e4d2e32571d7b2f12a27ea986145
XuPeiYao <st89382000@gmail.com>
2018-11-18 21:27:28 +0800

    加入Swagger可選參數操作過濾器

---
+++ XPY.Templates.Web/XPY.Templates.Web.Base/Utilities/Swagger/OptionParamProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using NSwag.SwaggerGeneration.Processors;
+using NSwag.SwaggerGeneration.Processors.Contexts;
+
+namespace $safeprojectname$.Utilities.Swagger {
+    /// <summary>
+    /// Swagger 可選參數操作過濾器
+    /// </summary>
+    public class OptionParamProcessor : IOperationProcessor {
+
+    public async Task<bool> ProcessAsync(OperationProcessorContext context) {
+        foreach (var param in context.Parameters.Values.ToList()) {
+            var paramInfo = context.MethodInfo.GetParameters().Single(x => x.Position == param.Position - 1);
+
+            if (paramInfo.HasDefaultValue &&
+                param.IsRequired &&
+                param.ActualParameter.Kind == NSwag.SwaggerParameterKind.Path &&
+                !context.OperationDescription.Path.Contains(param.Name)) {
+                param.IsRequired = false;
+            }
+        }
+        return true;
+    }
+}
+}
